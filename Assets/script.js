59e44b819c4d34073ff59cbb3027a825951fcd25
Thu Van <luuluu_1975@yahoo.com.au>
2020-12-02 20:52:33 +1100

    Updated code

--- Assets/script.js
+++ Assets/script.js
@@ -1,83 +1,98 @@
+//PasswordCriteria
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 
 var lengthMin = 8;
 var lengthMax = 128;
-var lowercase = true;"abcdefghijklmnopqrstuvwxyz";
-var uppercase = true; "ABCDEFGHIJKLMNOPQRSTUVWXY";
-var number = true; "0123456789";
-var specialCharacters = true; "!@#?$"; 
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXY";
+var number = "0123456789";
+var specialCharacters = "!@#?$"; 
+var selectedCharacters = ""
 var characters = (lowercase , uppercase, number, specialCharacters);
 
 //WHEN prompted for the length of the password
 //THEN I choose a length of at least 8 characters and no more than 128 characters
 // what length?, min 8 varchar and max 128 varchar
 // alert if below min or =128 or over or invalid type
+function generatePassword() {
+    console.log ("it works");
+var userLength=0 ;
+while (userlength >=lengthMin && userLength <=lengthMax){
+    userLength = prompt("Please a length between 8 and 128");
+    userLength=parseInt(userLength)
+}
+console.log(userLength)
+}
 
-var userLength = prompt("Please enter the length you want your password to be");
-if (userlength <=lengthMin && userLength >=lengthMax || userlength != integer);{
-alert("Password length must be between 8 and 128 characters.");
-} console.log(userLength)
 
-//THEN I am presented with a series of prompts for password criteria
 //WHEN prompted for character types to include in the password
 //THEN I choose lowercase, uppercase, numeric, and/or special characters
-var uservarchar = prompt("Would you like to include " + characters[i])
-for (var i = 0; i < characters.length; i++) 
-    console.log(lowerCase, upperCase, number, specialCharacters);
+// var uservarchar = prompt("Would you like to include " + characters[i]) //create array of questions
+// for (var i = 0; i < characters.length; i++) 
+//     console.log(lowerCase, upperCase, number, specialCharacters);
 
+    //click Generate
 
+//WHEN all prompts are answered
+//THEN a password is generated that matches the selected criteria
 //Generate Password Function
-function generatePassword("click",);
- if uservarchar.lowerCase = true,{
-   function getRandomLowerCase(),
-       return String.lowerCase(Math.floor(Math.random()*26)+97);
-}
- }
- if uservarchar.upperCase = true,{
-     function getRandomUpperCase(),
-        return String.upperCase(Math.floor(Math.random()*26)+65),
- }
- if uservarchar.number = true, {
-     function getRandomNumber(),
-        return String.number(Math.floor(Math.random()*10)+48),
-     }
- if uservarchar.specialCharacters = true,{
-     function getRandomSymbol(),
-        return String.specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
 
-for (var i = 0; i < userLendgth; i++)
-alert("Your new password is " + generatePassword)
+// for(var i = 0; i <userLength; i++){
+// function ();
+//  if uservarchar.lowerCase = true,{
+//    function getRandomLowerCase(),
+//        return String.lowerCase(Math.floor(Math.random()*26)+97),
+// }
+//  if uservarchar.upperCase = true,{
+//      function getRandomUpperCase(),
+//         return String.upperCase(Math.floor(Math.random()*26)+65),
+//  }
+//  if uservarchar.number = true, {
+//      function getRandomNumber(),
+//         return String.number(Math.floor(Math.random()*10)+48),
+//      }
+//  if uservarchar.specialCharacters = true,{
+//      function getRandomSymbol(),
+//         return String.specialCharacters[Math.floor(Math.random()*specialCharacters.length),
+//  }
+// }
 
-
-
-
-
-
-
-
-userLength = computerLength
-userVarchar = computerVarchar
-if (str.indexOf.lowercase)
-
-
-
-//Random Number selection
-for (var i = 0; i < 10; i++)
-// Randomly chooses a choice from the options array. This is the Computer's guess.
- var userNumber = computerLength[Math.floor(Math.random() * computerLength.length)];
-</body>
-
-console.log(userlength)
-
-
-
-
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-
-
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
 //WHEN all prompts are answered
 //THEN a password is generated that matches the selected criteria
 //WHEN the password is generated
 //THEN the password is either displayed in an alert or written to the page
+
+
+
+
+
+
+
+// userLength = computerLength
+// userVarchar = computerVarchar
+// if (str.indexOf.lowercase)
+
+
+
+// //Random Number selection
+// for (var i = 0; i < 10; i++)
+// // Randomly chooses a choice from the options array. This is the Computer's guess.
+//  var userNumber = computerLength[Math.floor(Math.random() * computerLength.length)];
+// </body>
+
+// console.log(userlength)
